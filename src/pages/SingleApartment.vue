3fff2a86404fcd1030b8eb711d3f208c76b2f22a
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-07-23 14:22:31 +0200

    invio emails

--- src/pages/SingleApartment.vue
+++ src/pages/SingleApartment.vue
@@ -6,7 +6,11 @@ export default{
     data(){
         return{
             baseUrl:'http://127.0.0.1:8000',
-            apartment: null
+            apartment: [],            
+            name: '',
+            email: '',
+            message: '',
+            success: false
         }
     },
     mounted(){
@@ -24,101 +28,131 @@ export default{
                     this.$router.push({name: 'NotFound'})
                 }
             })
+        },
+        sendForm() {
+
+            const data = {
+                name: this.name,
+                email: this.email,
+                message: this.message
+            }      
+
+
+            axios.post(`${this.baseUrl}/api/contacts`, data ).then( response => {
+
+                this.success = response.data.success;
+                if(this.success) {
+                    this.name = '',
+                    this.email = '',
+                    this.message = ''
+                }
+            });
+            
         }
     }
 }
 </script>
 
 <template>
-    <div v-if="apartment">
-
-        <h3>{{ apartment.title }}</h3>
-        
-
+    <div v-if="apartment" class="row">
         <div class="col-12 col-md-6 col-lg-6 text-center">
 
-                <div v-if="apartment.cover">
-                    <img class="img-fluid" src="{{ asset('storage/'. $apartment->cover) }}" alt="{{ $apartment->title }}">
-                </div>
+            <div v-if="apartment.cover">
+                <img class="img-fluid" :src="`${this.baseUrl}/storage/${apartment.cover}`" :alt="apartment.title">
+            </div>
                 
-            <div class="row justify-content-center mt-3">
+            <div class="row justify-content-center mt-3 gap-0">
 
-                {{-- per prendere le immagini usiamo la relazione con la tabella images --}}
-                @if($apartment->images)
-                <div class="carousel slide col-12 col-md-8 col-lg-8" id="carouselExampleAutoplaying" data-bs-ride="carousel">
+                <!-- carousel -->
+                <div v-if="apartment.images" class="carousel slide col-12 col-md-6 col-lg-6" id="carouselExampleAutoplaying" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        @foreach($apartment->images as $index => $e)
-                            <div class="carousel-item {{ $index === 0 ? 'active' : '' }}">
-                                <img src="{{ asset('storage/'. $e->url) }}" class="d-block w-100" alt="{{$e->id}}">
-                            </div>
-                        @endforeach
+
+                        <!-- images -->
+                        <div v-for="( elem, index ) in apartment.images" :key="index" class="carousel-item" :class="index === 0 ? 'active' : ''">                                
+                            <img class="d-block w-100" :src="`${this.baseUrl}/storage/${elem.url}`" :alt="apartment.title">
+                        </div>
                     </div>
+
+                    <!-- prev button -->
                     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="prev">
                         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Previous</span>
                     </button>
+
+                    <!-- next button -->
                     <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleAutoplaying" data-bs-slide="next">
                         <span class="carousel-control-next-icon" aria-hidden="true"></span>
                         <span class="visually-hidden">Next</span>
                     </button>
-                </div>
-                
-                        {{-- <div class=" p-0">
-                            <img class="img-fluid" src="{{ asset('storage/'. $elem->url) }}" alt="">
-                        </div> --}}
-                @endif
+                </div>                
             </div>
-            </div>
+        </div>
 
-            <div class="col-12 col-md-6 col-lg-6">
-            <h1 class="border-bottom">{{ $apartment->title }}</h1>
+
+        <div class="col-12 col-md-6 col-lg-6">
+            <h1 class="border-bottom">{{ apartment.title }}</h1>
             <div>
-                <p>{{ $apartment->description }}</p>
+                <p>{{ apartment.description }}</p>
                 <span class="d-block">
-                    numero stanza: {{ $apartment->rooms }}
+                    numero stanza: {{ apartment.rooms }}
                 </span>
                 <span class="d-block">
-                    numero stanze letto: {{ $apartment->bedrooms }}
+                    numero stanze letto: {{ apartment.bedrooms }}
                 </span>
                 <span class="d-block">
-                    numero bagni: {{ $apartment->bathrooms }}
+                    numero bagni: {{ apartment.bathrooms }}
                 </span>
                 <span class="d-block">
-                    metri quadri: {{ $apartment->square_meters }}mq
+                    metri quadri: {{ apartment.square_meters }}mq
                 </span>
                 <span class="d-block">
-                indirizzo: {{ $apartment->address }}
+                indirizzo: {{ apartment.address }}
+                </span>
+                <span v-if="apartment.price" class="d-block">
+                    prezzo: {{ apartment.price }}&euro;
                 </span>
                 <span class="d-block">
-                    prezzo: {{ $apartment->price }}&euro;
+                    visibilità: {{ (apartment.visibility) ? 'visibile' : 'non visibile' }}
                 </span>
-                <span class="d-block">
-                    visibilità: {{ ($apartment->visibility) ? 'visibile' : 'non visibile' }}
-                </span>
-                <h5 class="mt-2"> Servizi  </h5>
+                <h5 class="mt-2"> Servizi</h5>
 
-                @if ($apartment->services)
-                    @foreach($apartment->services as $elem)
-                        <span class="d-block mt-1"> <i class="fa-solid {{ $elem->icon }} me-1 "></i> {{  $elem->name }} </span>
-                    @endforeach
-                @endif
+                <span v-for="( elem, index ) in apartment.services" :key="index" class="d-block mt-1"> 
+                    <i class="fa-solid {{ elem.icon }} me-1 "></i> {{  elem.name }} 
+                </span>
+            </div>
+        </div>
 
-                <div class="d-flex column-gap-1 mt-2">
+    </div>
 
-                    {{-- edit --}}
-                    <a href="{{route('admin.edit', $apartment)}}"
-                        class="btn btn-primary"
-                    >
-                        Modifica
-                    </a>
+    <div class="container m-5">
+        <h3>Contattami</h3>
 
-                    {{-- delete --}}
-                    <form action="{{ route('admin.destroy', $apartment) }}" method="POST" onclick="return confirm(`Sicuro di voler eliminare l'appartamento?`)" >
-                        @csrf
-                        @method('DELETE')
-                        <button class="btn btn-danger" type="submit">Elimina</button>
-                    </form>
-                </div>
+        <div class="row">
+            <div class="col-12 col-md-6 col-lg-6">
+
+                <form action="" @submit.prevent="sendForm()">
+                    <div class="mb-3">
+                        <label for="" class="form-label">Nome</label>
+                        <input type="text" class="form-control" id="" placeholder="Nome" name="name" v-model="name">
+                    </div>
+    
+                    <div class="mb-3">
+                        <label for="" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="" placeholder="name@example.com" name="email" v-model="email">
+                    </div>
+    
+                    <div class="mb-3">
+                        <div class="mb-3">
+                            <label for="" class="form-label">Messaggio</label>
+                            <textarea class="form-control" id="" rows="3" name="message" v-model="message"></textarea>
+                        </div>
+                    </div>
+    
+                    <button type="submit" class="btn btn-primary">
+                        Invia
+                    </button>
+    
+                </form>
             </div>
         </div>
     </div>
