8daab4384f53b6c6e95a1b0df767228ad08990b5
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-07-24 14:53:49 +0200

    autocomplete

--- src/main.js
+++ src/main.js
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
 // import './style.css'
 import App from './App.vue'
-
+import { createPinia } from 'pinia';
 //importare bootstrap js
 import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 import router from "./router";
 
+const pinia = createPinia();
+const app = createApp(App);
 
-createApp(App).use(router).mount('#app')
+app.use(pinia);
+app.use(router);
+app.mount('#app')
