bc5f10cf8f19ca8c254ab2e10ea6aada4a2889cf
Your Namede <edoardo.monti.bo@gmail.com>
2023-07-25 12:25:40 +0200

    creazione componenete jumbotron

---
+++ src/components/JumboComp.vue
@@ -0,0 +1,25 @@
+<script>
+
+    
+    export default {
+        name:'JumboComp',
+        components: {
+            
+
+        },
+        data() {
+            return {
+
+            }
+        },
+    }
+</script>
+
+<template>
+    <h1>qua ci va il componenete</h1>
+
+</template>
+
+<style lang="scss">
+   
+</style>
--- src/pages/HomeComp.vue
+++ src/pages/HomeComp.vue
@@ -2,61 +2,57 @@
 import axios from 'axios';
 import ttServices from "@tomtom-international/web-sdk-services";
 
+import JumboComp from '../components/JumboComp.vue';
     export default {
-        name:'HomeComp',
-        data() {
-            return {
-                apartments:[],
-                address: '',
-                baseUrl: 'http://127.0.0.1:8000'
+    name: "HomeComp",
+    data() {
+        return {
+            apartments: [],
+            address: "",
+            baseUrl: "http://127.0.0.1:8000"
+        };
+    },
+    mounted() {
+        this.getApartment();
+    },
+    methods: {
+        getApartment() {
+            axios.get(`${this.baseUrl}/api/apartments`).then((res) => {
+                this.apartments = res.data.apartment.data;
+                console.log(res);
+            });
+        },
+        autocomplete() {
+            // Ottenimento dell'indirizzo dal campo input
+            const search = document.querySelector("#search");
+            if (search.value) {
+                ttServices.services.geocode({
+                    batchMode: "async",
+                    key: "74CVsbN34KoIljJqOriAYN2ZMEYU1cwO",
+                    query: search.value,
+                    countrySet: "IT",
+                    language: "it-IT",
+                }).then(function (response) {
+                    const results = response.results;
+                    console.log(results);
+                    // se abbiamo dei risultati ottenuti
+                    if (results.length) {
+                        for (const elem of results) {
+                            document.getElementById("datalistOptions").innerHTML += `<option value="${elem.address.freeformAddress}">${elem.address.freeformAddress}</option>`;
+                        }
+                    }
+                });
             }
         },
-        mounted(){
-            this.getApartment()
-        },
-        methods: {
-            getApartment(){
-                axios.get(`${this.baseUrl}/api/apartments`).then((res) =>{
-                    this.apartments = res.data.apartment.data
-                    console.log(res)
-                })
-            },
-            autocomplete() {    
-                // Ottenimento dell'indirizzo dal campo input
-                const search = document.querySelector('#search');
-            
-                if( search.value ) {
-
-                    ttServices.services.geocode({
-                        batchMode: 'async',
-                        key: "74CVsbN34KoIljJqOriAYN2ZMEYU1cwO",
-                        query: search.value,
-                        countrySet: 'IT',
-                        language: 'it-IT',
-                    }).then(
-                        function (response) {
-                            
-                            const results = response.results;
-                            console.log(results)                
-            
-                            // se abbiamo dei risultati ottenuti
-                            if (results.length)  {   
-            
-                                for (const elem of results) {
-                                    document.getElementById('datalistOptions').innerHTML += `<option value="${elem.address.freeformAddress}">${elem.address.freeformAddress}</option>`;
-                                }
-                            }
-                            
-                        }
-                    );
-                } 
-            },
-        }
-
+    },
+    components: { 
+        JumboComp 
     }
+}
 </script>
 <template>
     <h1>home page</h1>
+    <JumboComp/>
     <div v-if="apartments" class="container">
         <div class="row">
 
