63b19c5344d08998b0a24567dea6c24cbfd10e23
TullioTambone <tulliotambone97@gmail.com>
2023-07-31 15:54:45 +0200

    homepage vue

--- src/pages/HomeComp.vue
+++ src/pages/HomeComp.vue
@@ -104,10 +104,360 @@ import TeamComp from '../components/TeamComp.vue';
             />
         </div>
     </div>
-   
+   <section class="container-fluid border-top">
+        
+        <!-- {{-- dashboard --}} -->
+        <div id="dashboard">
+            <div class="container pt-5">
+                <div class="row">
+
+                    <!-- {{-- description --}} -->
+                    <div class="box-description col-12 col-md-8 col-lg-6 d-flex justify-content-center align-items-center">
+                        <div class="d-flex flex-column">
+                            <div class="info d-flex justify-content-center align-items-center">
+                                <div class="w-75">
+                                    <h2>Esplora le case vacanza</h2>
+                                    <p>
+                                        Naviga tra le nostre incantevoli opzioni di case vacanza disponibili in destinazioni mozzafiato in tutto il mondo. Dalle rilassanti spiagge tropicali ai pittoreschi rifugi di montagna, troverai sicuramente la casa perfetta per il tuo prossimo soggiorno indimenticabile.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- {{-- img --}} -->
+                    <div class="box-img col-12 col-md-4 col-lg-6 d-flex justify-content-center align-items-center">
+                        <img src="/img/Boolbnb-logo.png" alt="">
+                    </div>
+                </div>
+
+                <!-- {{-- benvenuto --}} -->
+                <div class="row mb-5">
+                    <div class="box-img col-12 col-md-4 col-lg-6 d-flex justify-content-center align-items-center">
+                        <img src="/img/Boolbnb-logo.png" alt="logo boolb&b">
+                    </div>
+                    <div class="box-description col-12 col-md-8 col-lg-6 d-flex justify-content-center align-items-center">
+                        <div class="d-flex flex-column">
+                            <h2>
+                                Benvenuto Prenota su BoolBnB: è facile e conveniente!
+                            </h2>
+                            <ul class="p-0">
+                                <li>
+                                    <div class="d-flex align-items-center single-cont">
+                                        <img src="/img/support.png" alt="">
+                                        <div class="info d-flex flex-column justify-content-center">
+                                            <h4>Supporto clienti pronto ad aiutarti</h4>
+                                            <p>Il nostro team di supporto è disponibile 24/7 per assisterti in ogni tua
+                                                esigenza.</p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="d-flex align-items-center single-cont">
+                                        <img src="/img/discount.jpeg" alt="">
+                                        <div class="info d-flex flex-column justify-content-center">
+                                            <h4>Accedi a sconti immediati</h4>
+                                            <p>Con le tariffe per soli iscritti risparmi in media un 15% su migliaia di hotel.
+                                            </p>
+                                        </div>
+                                    </div>
+                                </li>
+                                <li>
+                                    <div class="d-flex align-items-center single-cont">
+                                        <img src="/img/calendar.png" alt="">
+                                        <div class="info d-flex flex-column justify-content-center">
+                                            <h4>Cancellazione gratuita</h4>
+                                            <p>Prenotazione flessibile per gran parte degli hotel*.</p>
+                                        </div>
+                                    </div>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="box-description col-12 col-md-8 col-lg-6 d-flex justify-content-center align-items-center">
+                        <div class="d-flex flex-column">
+                            <div class="info d-flex justify-content-center align-items-center">
+                                <div class="w-75">
+                                    <h2>Invia una richiesta personalizzata</h2>
+                                    <ul class="p-0">
+                                        <li>
+                                            <div class="d-flex align-items-center single-cont">
+                                                <img src="/img/contact.png" alt="">
+                                                <div class="info d-flex flex-column justify-content-center">
+                                                    <h4>Contatta l'Host</h4>
+                                                    <p>Una volta trovata la casa che cattura il tuo cuore, non esitare a contattare
+                                                        l'host.</p>
+                                                </div>
+                                            </div>
+                                        </li>
+                                        <li>
+                                            <div class="d-flex align-items-center single-cont">
+                                                <img src="/img/custom.png" alt="">
+                                                <div class="info d-flex flex-column justify-content-center">
+                                                    <h4>Personalizza la tua esperienza</h4>
+                                                    <p>L'host sarà lieto di aiutarti a personalizzare il tuo soggiorno secondo le tue
+                                                        preferenze. Che si tratti di suggerimenti su attrazioni locali, consigli sui
+                                                        ristoranti o servizi aggiuntivi, siamo qui per rendere la tua vacanza unica e
+                                                        indimenticabile.</p>
+                                                </div>
+                                            </div>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="box-img col-12 col-md-4 col-lg-6 d-flex justify-content-center align-items-center">
+                        <img src="/img/Boolbnb-logo.png" alt="">
+                    </div>
+                </div>
+
+                <div class="row mb-5">
+                    <div class="box-description col-12 d-flex justify-content-center align-items-center">
+                        <div class="d-flex flex-column w-75 text-center">
+                            <h2 class="text-center">Scopri i Piani di Abbonamento: Metti in Evidenza la Tua Casa Vacanza!</h2>
+                            <p>"Rendi il tuo appartamento un vero protagonista con i nostri esclusivi piani di abbonamento!
+                                Aumenta la visibilità del tuo annuncio, posizionandolo in cima alle ricerche degli ospiti.
+                                Scopri le opzioni su misura per te e attira più prenotazioni creando un'esperienza eccezionale
+                                per i tuoi futuri ospiti. Esplora i piani disponibili e raggiungi un pubblico più ampio oggi
+                                stesso!"</p>
+                        </div>
+                    </div>
+
+                    <div class="card-sub d-flex justify-content-center">
+                        <div class="card card__one">
+                            <div class="card__text">
+                                <h3>€2.99</h3>
+                                <span>24h</span>
+                                <span>BASE</span>
+                                <hr>
+                                <p class="card__title"></p>
+                                <div>
+                                    <ul class="features">
+                                        <li>
+                                            <span class="icon">
+                                                <i class="fa-solid fa-check"></i>
+                                            </span>
+                                            <span><strong>1</strong> giorno di visibilità</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card card__two">
+                            <div class="card__text">
+                                <h3>€5.99</h3>
+                                <span>48h</span>
+                                <span>PREMIUM</span>
+                                <hr>
+                                <p class="card__title"></p>
+                                <div>
+                                    <ul class="features">
+                                        <li>
+                                            <span class="icon">
+                                                <i class="fa-solid fa-check"></i>
+                                            </span>
+                                            <span><strong>2</strong> giorni di visibilità</span>
+                                        </li>
+                                        <li>
+                                            <span class="icon">
+                                                <i class="fa-solid fa-check"></i>
+                                            </span>
+                                            <span><strong>Badge</strong> "Premium"</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card card__three">
+                            <div class="card__text">
+                                <h3>€9.99</h3>
+                                <span>144h</span>
+                                <span>PLATINO</span>
+                                <hr>
+                                <p class="card__title"></p>
+                                <div>
+                                    <ul class="features">
+                                        <li>
+                                            <span class="icon">
+                                                <i class="fa-solid fa-check"></i>
+                                            </span>
+                                            <span><strong>6</strong> giorni di visibilità</span>
+                                        </li>
+                                        <li>
+                                            <span class="icon">
+                                                <i class="fa-solid fa-check"></i>
+                                            </span>
+                                            <span>Premium <strong>x3</strong></span>
+                                        </li>
+                                        <li>
+                                            <span class="icon">
+                                                <i class="fa-solid fa-check"></i>
+                                            </span>
+                                            <span><strong>Badge</strong> "Platino"</span>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </section>
 </template>
 
 <style lang="scss" scoped>
 @import '../style/main.scss';
 
+
+#dashboard{
+    .container{
+        .row{
+            margin-bottom: 100px;
+            .card-sub{
+                margin-top: 50px;
+                .card {
+                    border-radius: 15px;
+                    box-shadow: 5px 5px 20px -5px rgba(0,0,0,0.6);
+                    height: 300px;
+                    overflow: hidden;
+                    width: 250px;
+                    text-align: center;
+                    margin: 0 30px;
+                     &:hover {
+                       box-shadow: 5px 5px 20px -7px rgba(0,0,0,0.5);
+                     }
+                     .card__text{
+                        h3{
+                            padding-top: 40px;
+                            font-size: 40px;
+                        }
+                        span{
+                            display: block;
+                        }
+                        hr{
+                            margin: 10px auto;
+                            width: 25%;
+                            border: 2px solid blue;
+                        }
+                        .features {
+                            display: flex;
+                            flex-direction: column;
+                            li {
+                                display: flex;
+                                align-items: center;
+                                gap: .5rem;
+                            }
+                            
+                            .icon {
+                                display: inline-flex;
+                                align-items: center;
+                                justify-content: center;
+                                width: 20px;
+                                height: 20px;
+                                svg {
+                                    width: 14px;
+                                    height: 14px;
+                                }
+                            }
+                            
+                            & + * {
+                                margin-top: 1.25rem;
+                            }
+                        }
+                     }
+                  }
+
+            }
+
+            .box-img{
+                img{
+                    width: 300px;
+                }
+                // border: 1px solid black;
+            }
+            .box-description{
+                h2{
+                    margin: 50px 0;
+                    font-weight: 600;
+                }
+                ul{
+                    li{
+                        margin-bottom: 20px;
+                        list-style: none;
+                        img{
+                            width: 60px;
+                        }
+                        .info{
+                            margin-left: 20px;
+                            h4{
+                                padding-top: 15px;
+                            }
+                            p{
+        
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+@media (max-width: 425px) {
+    #dashboard{
+        .container{
+            .row{
+                .card-sub{
+                    flex-direction: column;
+                    justify-content: center;
+                    align-items: center;
+                    order: 3;
+                    .card{
+                        margin-bottom: 30px;
+                    }
+                }
+                .box-img{
+                   order: 1;
+                    
+                }
+                .box-description{
+                    order: 2;
+                    ul{
+                        padding-left: 0;
+                        li{
+                            .single-cont{
+                                text-align: center;
+                                flex-direction: column;
+                                align-items: center;
+                                justify-content: center;
+                                .info{
+                                    margin: 0 auto;
+                                    h4{
+                                        padding-top: 15px;
+                                    }
+                                    p{
+                
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@media (max-width: 1024px) {
+   
+}
 </style>
