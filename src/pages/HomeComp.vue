5fa7f9c6f7781b09ac175b1068ba6b77b215a6fa
TullioTambone <tulliotambone97@gmail.com>
2023-07-31 19:35:04 +0200

    adj home

--- src/pages/HomeComp.vue
+++ src/pages/HomeComp.vue
@@ -110,9 +110,9 @@ import TeamComp from '../components/TeamComp.vue';
                         <div class="d-flex flex-column">
                             <div class="info d-flex justify-content-center align-items-center">
                                 <div class="w-75">
-                                    <h2>Esplora le case vacanza</h2>
+                                    <h2>Trova la Casa dei Tuoi Sogni</h2>
                                     <p>
-                                        Naviga tra le nostre incantevoli opzioni di case vacanza disponibili in destinazioni mozzafiato in tutto il mondo. Dalle rilassanti spiagge tropicali ai pittoreschi rifugi di montagna, troverai sicuramente la casa perfetta per il tuo prossimo soggiorno indimenticabile.
+                                        Naviga tra i nostri appartamenti disponibili in destinazioni mozzafiato in tutto il mondo. Dalle rilassanti spiagge tropicali ai pittoreschi rifugi di montagna, troverai sicuramente la casa perfetta per il tuo prossimo soggiorno indimenticabile.
                                     </p>
                                 </div>
                             </div>
@@ -132,7 +132,7 @@ import TeamComp from '../components/TeamComp.vue';
                     <div class="box-description col-12 col-md-8 col-lg-6 d-flex justify-content-center align-items-center">
                         <div class="d-flex flex-column">
                             <h2>
-                                Benvenuto Prenota su BoolBnB: è facile e conveniente!
+                                Scopri BoolB&B è facile e intuitivo!
                             </h2>
                             <ul class="p-0">
                                 <li>
@@ -147,20 +147,21 @@ import TeamComp from '../components/TeamComp.vue';
                                 </li>
                                 <li>
                                     <div class="d-flex align-items-center single-cont">
-                                        <img src="/img/discount.jpeg" alt="">
+                                        <img src="/img/contact.png" alt="">
                                         <div class="info d-flex flex-column justify-content-center">
-                                            <h4>Accedi a sconti immediati</h4>
-                                            <p>Con le tariffe per soli iscritti risparmi in media un 15% su migliaia di hotel.
-                                            </p>
+                                            <h4>Contatta l'Host</h4>
+                                            <p>Una volta trovata la casa che cattura il tuo cuore, non esitare a contattare
+                                                l'host.</p>
                                         </div>
                                     </div>
                                 </li>
                                 <li>
                                     <div class="d-flex align-items-center single-cont">
-                                        <img src="/img/calendar.png" alt="">
+                                        <img src="/img/custom.png" alt="">
                                         <div class="info d-flex flex-column justify-content-center">
-                                            <h4>Cancellazione gratuita</h4>
-                                            <p>Prenotazione flessibile per gran parte degli hotel*.</p>
+                                            <h4>Personalizza la tua esperienza</h4>
+                                            <p>L'host sarà lieto di aiutarti a personalizzare la tua ricerca secondo le tue
+                                                preferenze. Siamo qui per aiutarti a trovare la casa fatta su misura per te.</p>
                                         </div>
                                     </div>
                                 </li>
@@ -169,45 +170,6 @@ import TeamComp from '../components/TeamComp.vue';
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="box-description col-12 col-md-8 col-lg-6 d-flex justify-content-center align-items-center">
-                        <div class="d-flex flex-column">
-                            <div class="info d-flex justify-content-center align-items-center">
-                                <div class="w-75">
-                                    <h2>Invia una richiesta personalizzata</h2>
-                                    <ul class="p-0">
-                                        <li>
-                                            <div class="d-flex align-items-center single-cont">
-                                                <img src="/img/contact.png" alt="">
-                                                <div class="info d-flex flex-column justify-content-center">
-                                                    <h4>Contatta l'Host</h4>
-                                                    <p>Una volta trovata la casa che cattura il tuo cuore, non esitare a contattare
-                                                        l'host.</p>
-                                                </div>
-                                            </div>
-                                        </li>
-                                        <li>
-                                            <div class="d-flex align-items-center single-cont">
-                                                <img src="/img/custom.png" alt="">
-                                                <div class="info d-flex flex-column justify-content-center">
-                                                    <h4>Personalizza la tua esperienza</h4>
-                                                    <p>L'host sarà lieto di aiutarti a personalizzare il tuo soggiorno secondo le tue
-                                                        preferenze. Che si tratti di suggerimenti su attrazioni locali, consigli sui
-                                                        ristoranti o servizi aggiuntivi, siamo qui per rendere la tua vacanza unica e
-                                                        indimenticabile.</p>
-                                                </div>
-                                            </div>
-                                        </li>
-
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="box-img col-12 col-md-4 col-lg-6 d-flex justify-content-center align-items-center">
-                        <img src="/img/Boolbnb-logo.png" alt="">
-                    </div>
-                </div>
 
                 <div class="row mb-5">
                     <div class="box-description col-12 d-flex justify-content-center align-items-center">
@@ -215,8 +177,7 @@ import TeamComp from '../components/TeamComp.vue';
                             <h2 class="text-center">Scopri i Piani di Abbonamento: Metti in Evidenza la Tua Casa Vacanza!</h2>
                             <p>"Rendi il tuo appartamento un vero protagonista con i nostri esclusivi piani di abbonamento!
                                 Aumenta la visibilità del tuo annuncio, posizionandolo in cima alle ricerche degli ospiti.
-                                Scopri le opzioni su misura per te e attira più prenotazioni creando un'esperienza eccezionale
-                                per i tuoi futuri ospiti. Esplora i piani disponibili e raggiungi un pubblico più ampio oggi
+                                Scopri le opzioni su misura per te e attira più futuri acquirenti. Esplora i piani disponibili e raggiungi un pubblico più ampio oggi
                                 stesso!"</p>
                         </div>
                     </div>
@@ -266,6 +227,12 @@ import TeamComp from '../components/TeamComp.vue';
                                             <span class="icon">
                                                 <i class="fa-solid fa-check"></i>
                                             </span>
+                                            <span>Base <strong>x2</strong></span>
+                                        </li>
+                                        <li>
+                                            <span class="icon">
+                                                <i class="fa-solid fa-check"></i>
+                                            </span>
                                             <span><strong>Badge</strong> "Premium"</span>
                                         </li>
                                     </ul>
@@ -310,9 +277,9 @@ import TeamComp from '../components/TeamComp.vue';
         </div>
    </section>
    <section class="py-5 border-top">
-    <div class="py-2">
-        <TeamComp/>
-    </div>
+        <div class="py-2 mt-5">
+            <TeamComp/>
+        </div>
 
    </section>
 </template>
